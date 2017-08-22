ed26277a2d0e2ffe1c5e3510fff8b3d70a64f7c9
Marcus Borkenhagen <m@fritschy.de>
2017-08-22 16:43:48 +0200

    approximate log, clang-format

--- main.cpp
+++ main.cpp
@@ -29,6 +29,214 @@
 
 namespace {
 
+namespace approximate {
+/* Approximation of f(x) = log(x)
+ * on interval [ 0.001, 0.01 ]
+ * with a polynomial of degree 3. */
+float log001(float x) {
+   float u = 4.9237523e+6f;
+   u = u * x + -1.0793186e+5f;
+   u = u * x + 8.9655674e+2f;
+   return u * x + -7.6570452f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 0.01, 0.1 ]
+ * with a polynomial of degree 3. */
+float log01(float x) {
+   float u = 4.9237523e+3f;
+   u = u * x + -1.0793186e+3f;
+   u = u * x + 8.9655674e+1f;
+   return u * x + -5.3544601f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 0.1, 1 ]
+ * with a polynomial of degree 3. */
+float log1(float x) {
+   float u = 4.9237523f;
+   u = u * x + -1.0793186e+1f;
+   u = u * x + 8.9655674f;
+   return u * x + -3.051875f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 1, 10 ]
+ * with a polynomial of degree 3. */
+float log10(float x) {
+   float u = 4.9237523e-3f;
+   u = u * x + -1.0793186e-1f;
+   u = u * x + 8.9655674e-1f;
+   return u * x + -7.4928994e-1f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 10, 100 ]
+ * with a polynomial of degree 3. */
+float log100(float x) {
+   float u = 4.9237523e-6f;
+   u = u * x + -1.0793186e-3f;
+   u = u * x + 8.9655674e-2f;
+   return u * x + 1.5532951f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 100, 1000 ]
+ * with a polynomial of degree 3. */
+float log1000(float x) {
+   float u = 4.9237523e-9f;
+   u = u * x + -1.0793186e-5f;
+   u = u * x + 8.9655674e-3f;
+   return u * x + 3.8558802f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 1000, 10000 ]
+ * with a polynomial of degree 3. */
+float log10000(float x) {
+   float u = 4.9237523e-12f;
+   u = u * x + -1.0793186e-7f;
+   u = u * x + 8.9655674e-4f;
+   return u * x + 6.1584653f;
+}
+
+/* Approximation of f(x) = log(x)
+ * on interval [ 1/1024, 1/128 ]
+ * with a polynomial of degree 3. */
+float log0128(float x) {
+   float u = 8.4613609e+6f;
+   u = u * x + -1.5063532e+5f;
+   u = u * x + 1.0390709e+3f;
+   return u * x + -7.7798414f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 1/128, 1/32 ]
+ * with a polynomial of degree 3. */
+float log032(float x) {
+   float u = 6.580625e+4f;
+   u = u * x + -5.474308e+3f;
+   u = u * x + 1.8864233e+2f;
+   return u * x + -6.0164018f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 1/32, 1/8 ]
+ * with a polynomial of degree 3. */
+float log08(float x) {
+   float u = 1.0282226e+3f;
+   u = u * x + -3.4214425e+2f;
+   u = u * x + 4.7160582e+1f;
+   return u * x + -4.6301074f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 1/8, 1/2 ]
+ * with a polynomial of degree 3. */
+float log02(float x) {
+   float u = 1.6065979e+1f;
+   u = u * x + -2.1384016e+1f;
+   u = u * x + 1.1790146e+1f;
+   return u * x + -3.2438131f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 1/2, 2 ]
+ * with a polynomial of degree 3. */
+float log2(float x) {
+   float u = 2.5103092e-1f;
+   u = u * x + -1.336501f;
+   u = u * x + 2.9475364f;
+   return u * x + -1.8575187f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 2, 8 ]
+ * with a polynomial of degree 3. */
+float log8(float x) {
+   float u = 3.9223581e-3f;
+   u = u * x + -8.3531312e-2f;
+   u = u * x + 7.3688409e-1f;
+   return u * x + -4.7122433e-1f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 8, 32 ]
+ * with a polynomial of degree 3. */
+float log32(float x) {
+   float u = 6.1286846e-5f;
+   u = u * x + -5.220707e-3f;
+   u = u * x + 1.8422102e-1f;
+   return u * x + 9.1507003e-1f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 32, 128 ]
+ * with a polynomial of degree 3. */
+float log128(float x) {
+   float u = 9.5760696e-7f;
+   u = u * x + -3.2629419e-4f;
+   u = u * x + 4.6055256e-2f;
+   return u * x + 2.3013644f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 128, 1024 ]
+ * with a polynomial of degree 3. */
+float log1024(float x) {
+   float u = 3.7575991e-9f;
+   u = u * x + -8.7681296e-6f;
+   u = u * x + 7.9274819e-3f;
+   return u * x + 4.0036607f;
+}
+/* Approximation of f(x) = log(x)
+ * on interval [ 1024, 8192 ]
+ * with a polynomial of degree 3. */
+float log8192(float x) {
+   float u = 7.3390607e-12f;
+   u = u * x + -1.3700203e-7f;
+   u = u * x + 9.9093524e-4f;
+   return u * x + 6.0831023f;
+}
+
+float log(float x) {
+   // XXX
+   /* union { */
+   /*    uint32_t u; */
+   /*    float f; */
+   /* } l; */
+   /* l.f = x; */
+   /* switch ((signed int)(((l.u >> 23) & 0xff) - 127)) { */
+   /*    case -9: case -8: case -7: return log0128(x); */
+   /*    case -6: case -5: return log032(x); */
+   /*    case -4: case -3: return log08(x); */
+   /*    case -2: case -1: return log02(x); */
+   /*    case 0: case 1: return log2(x); */
+   /*    case 2: case 3: return log8(x); */
+   /*    case 4: case 5: return log32(x); */
+   /*    case 6: case 7: return log128(x); */
+   /*    case 8: case 9: case 10: return log1024(x); */
+   /*    case 11: case 12: case 13: return log8192(x); */
+   /* } */
+
+   // XXX
+   /* if (x < 1.0 / 128.0) return log0128(x); */
+   /* if (x < 1.0 / 32.0) return log032(x); */
+   /* if (x < 1.0 / 8.0) return log08(x); */
+   /* if (x < 1.0 / 2.0) return log02(x); */
+   /* if (x < 2.0) return log2(x); */
+   /* if (x < 8.0) return log8(x); */
+   /* if (x < 32.0) return log32(x); */
+   /* if (x < 128.0) return log128(x); */
+   /* if (x < 1024.0) return log1024(x); */
+   /* if (x < 8192.0) return log8192(x); */
+
+   // XXX
+   if (x < 0.01)
+      return log001(x);
+   if (x < 0.1)
+      return log01(x);
+   if (x < 1)
+      return log1(x);
+   if (x < 10)
+      return log10(x);
+   if (x < 100)
+      return log100(x);
+   if (x < 1000)
+      return log1000(x);
+   if (x < 10000)
+      return log10000(x);
+
+   return glm::log(x);
+}
+}
+
 using namespace glm;
 
 constexpr int AA = 1;
@@ -37,13 +245,15 @@ float iGlobalTime;
 vec2 iResolution;
 vec4 c;
 
-float map(vec3 p, vec4 &oTrap) {
+float map(vec3 p, vec4 *oTrap) {
    vec4 z = vec4(p, 0.0f);
    float md2 = 1.0f;
    float mz2 = dot(z, z);
    vec4 nz;
 
-   vec4 trap = vec4(abs(z.xyz()), dot(z, z));
+   vec4 trap;
+   if (oTrap)
+      trap = vec4(abs(z.xyz()), dot(z, z));
 
    for (int i = 0; i < 11; i++) {
       // |dz|^2 -> 4*|dz|^2
@@ -57,7 +267,8 @@ float map(vec3 p, vec4 &oTrap) {
       nz.w = yzw.z;
       z = nz + c;
 
-      trap = min(trap, vec4(abs(z.xyz()), dot(z, z)));
+      if (oTrap)
+         trap = min(trap, vec4(abs(z.xyz()), dot(z, z)));
 
       mz2 = dot(z, z);
       if (mz2 > 4.0f) {
@@ -65,9 +276,16 @@ float map(vec3 p, vec4 &oTrap) {
       }
    }
 
-   oTrap = trap;
+   /* printf("%f\n", mz2 / md2); */
 
-   return 0.25f * sqrt(mz2 / md2) * log(mz2);
+   float ret = 0.25f * sqrt(mz2 / md2) * approximate::log(mz2);
+
+   if (oTrap)
+      *oTrap = trap;
+
+   /* if (ret < 10) */
+   /*    printf("%f\n", mz2); */
+   return ret;
 }
 
 // analytical normal for quadratic formula
@@ -111,12 +329,14 @@ float intersect(vec3 ro, vec3 rd, vec4 &res) {
    float maxd = 10.0f;
    float h = 1.0f;
    float t = 0.0f;
-   for (int i = 0; i < 150; i++) {
+   int i;
+   for (i = 0; i < 150; i++) {
       if (h < 0.002f || t > maxd)
          break;
-      h = map(ro + rd * t, tmp);
+      h = map(ro + rd * t, &tmp);
       t += h;
    }
+   /* printf("%d\n", i); */
    if (t < maxd) {
       resT = t;
       res = tmp;
@@ -129,8 +349,7 @@ float softshadow(vec3 ro, vec3 rd, float mint, float k) {
    float res = 1.0f;
    float t = mint;
    for (int i = 0; i < 64; i++) {
-      vec4 kk;
-      float h = map(ro + rd * t, kk);
+      float h = map(ro + rd * t, NULL);
       res = min(res, k * h / t);
       if (res < 0.001f)
          break;
@@ -225,19 +444,22 @@ void frame(enki::TaskScheduler *ts, std::vector<uint32_t> *buf, int w, int h) {
 
 #ifdef USE_ENKITS
 #define TILE_SIZE 8
-   enki::TaskSet t(h / TILE_SIZE + h % TILE_SIZE, [h, w, ro, ta, cr, buf](enki::TaskSetPartition range, uint32_t) {
-      vec4 fragColor;
-      for (unsigned y = range.start * TILE_SIZE,
-                    yend = min(int(h), int(range.end * TILE_SIZE));
-                    y < yend; y++)
-         for (unsigned x = 0; x < w; x++) {
-            mainImage(ro, ta, cr, fragColor, vec2(x, y));
-            fragColor = clamp(fragColor, vec4(0), vec4(1));
-            fragColor *= 255.f;
-            (*buf)[(h - 1 - y) * w + x] = MFB_RGB(
-               uint8_t(fragColor.r), uint8_t(fragColor.g), uint8_t(fragColor.b));
-         }
-   });
+   enki::TaskSet t(
+      h / TILE_SIZE + h % TILE_SIZE,
+      [h, w, ro, ta, cr, buf](enki::TaskSetPartition range, uint32_t) {
+         vec4 fragColor;
+         for (unsigned y = range.start * TILE_SIZE,
+                       yend = min(int(h), int(range.end * TILE_SIZE));
+              y < yend; y++)
+            for (unsigned x = 0; x < w; x++) {
+               mainImage(ro, ta, cr, fragColor, vec2(x, y));
+               fragColor = clamp(fragColor, vec4(0), vec4(1));
+               fragColor *= 255.f;
+               (*buf)[(h - 1 - y) * w + x] =
+                  MFB_RGB(uint8_t(fragColor.r), uint8_t(fragColor.g),
+                          uint8_t(fragColor.b));
+            }
+      });
    ts->AddTaskSetToPipe(&t);
 
    ts->WaitforAll();
@@ -285,9 +507,7 @@ int main(int argc, char **argv) {
       static float t_offset = seconds();
       return seconds() - t_offset;
    };
-   std::function<bool()> should_continue = [] {
-      return 1;
-   };
+   std::function<bool()> should_continue = [] { return 1; };
 
    // I am a lazy person...
    if (argv[1]) {
